9193305d8c98c324b9709dedcb5550143166d20c
caiovicto546 <caiovicto@gmail.com>
2019-01-16 13:07:12 -0200

    Criado Classe PosicaoXadrez para converter a possição citada pelo usuário para a posição da matriz

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xadrez.tabuleiro;
 using Xadrez.Pecas;
+using Xadrez.Exception;
 
 namespace Xadrez
 {
@@ -8,17 +9,30 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab;
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.ToPosicao());
+
+
+            /*Tabuleiro tab;
 
             tab = new Tabuleiro(8, 8);
 
-            tab.ColocarPeca(new Torre(Enum.Cor.Preto, tab), new Posicao(0,0));
-            tab.ColocarPeca(new Torre(Enum.Cor.Preto, tab), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(Enum.Cor.Preto, tab), new Posicao(2, 4));
+            try
+            {
+                tab.ColocarPeca(new Torre(Enum.Cor.Preto, tab), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(Enum.Cor.Preto, tab), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(Enum.Cor.Preto, tab), new Posicao(2, 4));
 
-            Tela.ExibeTabuleiro(tab);
-
-
+                Tela.ExibeTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            */
             Console.ReadLine();
             
         }
---
+++ Xadrez/tabuleiro/PosicaoXadrez.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xadrez.tabuleiro
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.Coluna = coluna;
+            this.Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
